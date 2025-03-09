58766b04d2eadfa6fb82620a083eef5fa14a9b3d
Vinicus-dot <vinicius.rs97@ufu.br>
2025-03-09 16:55:52 -0300

    protect the routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,9 @@ import Home from '@/views/Home.vue'
 import Auth from '@/views/Auth.vue'
 import Products from '@/views/Products.vue'
 import Orders from '@/views/Orders.vue'
+import { jwtVerify } from 'jose'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,14 +22,38 @@ const router = createRouter({
     {
       path: '/products',
       name: 'products',
-      component: Products
+      component: Products,
+      meta: { requiresAuth: true }
     },
     {
       path: '/orders',
       name: 'orders',
-      component: Orders
+      component: Orders,
+      meta: { requiresAuth: true }
     }
   ],
 })
+
+async function isAuthenticated() {
+  const token = localStorage.getItem('token');
+  const jwtSecret = import.meta.env.VITE_JWT_SECRET;
+  
+  if (!token || !jwtSecret) return false;
+
+  try {
+    const secret = new TextEncoder().encode(jwtSecret);
+    await jwtVerify(token, secret);
+    return true;
+  } catch (error) {
+    return false;   
+  }
+}
+
+router.beforeEach(async (to, from) => {
+  if (to.meta.requiresAuth && !await isAuthenticated()) {
+    return '/auth'
+  }
+  return true
+});
 
 export default router
